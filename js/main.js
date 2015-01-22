6c36336eb0d2c574dd9c4996f7f3a5efe10d8823
Masato Seki <sekkithub@gmail.com>
2015-01-22 10:48:17 +0000

    fix third feature conflict

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 console.log('first feature');
+<<<<<<< HEAD
+=======
+console.log('second feature from work');
+console.log('third feature from work');
+>>>>>>> ab43507ded4eda39f812a380291956367a991b43
