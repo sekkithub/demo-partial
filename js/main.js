46c097fcb0cd1e60b4aeef855c28552eb9750d73
Masato Seki <sekkithub@gmail.com>
2015-01-22 11:48:38 +0000

    delete old console.log on main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,2 @@
-console.log('first feature');
-console.log('second feature from work');
-console.log('third feature from work');
-console.log('second feature from work');
-console.log('second feature from home');
-console.log('third feature from work');
-
 console.log('home-new-feature-01');
 console.log('new feature at work');
